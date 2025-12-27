0b52f05615d2a4190060fc4a86751fb43202a19e
indian-chutney <zubairshaik2615@gmail.com>
2025-12-27 21:42:07 +0530

    AdjacencyList fix

--- Graphs/AdjacencyList.java
+++ Graphs/AdjacencyList.java
@@ -1,35 +1,34 @@
+import java.util.List;
 import java.util.ArrayList;
 
 public class AdjacencyList {
     public boolean isDi;
-    public ArrayList<Integer>[] AdjList;
+    public List<List<Integer>> AdjList;
 
     AdjacencyList(boolean isDi, int vertices) {
         this.isDi = isDi;
-        this.AdjList = (ArrayList<Integer>[]) new ArrayList[vertices];
+        this.AdjList = new ArrayList<>(vertices);
+
+        for(int i = 0; i < vertices; i++){
+            this.AdjList.add(new ArrayList<Integer>());
+        }
     }
 
     public void addEdge(int u, int v){
-        if(AdjList[u] == null){
-            AdjList[u] = new ArrayList<Integer>();
-        }
-        AdjList[u].add(v);
+        AdjList.get(u).add(v);
         if(!isDi){
-            if(AdjList[v] == null){
-                AdjList[v] = new ArrayList<Integer>();
-            }
-            AdjList[v].add(u);
+            AdjList.get(v).add(u);
         }
     }
 
     public void printGraph(){
-        for(int i = 0; i < AdjList.length; i++){
+        for(int i = 0; i < AdjList.size(); i++){
             System.out.print(i + " : ");
-            if(AdjList[i] == null) {
+            if(AdjList.get(i).isEmpty()) {
                 System.out.println("NULL");
                 continue;
             }
-            for (Integer val: AdjList[i]){
+            for (Integer val: AdjList.get(i)){
                 System.out.print(val + ", ");
             }
             System.out.println();
@@ -37,11 +36,11 @@ public class AdjacencyList {
     }
     
     public static void main(String args[]){
-        AdjacencyList adj = new AdjacencyList(false, 3);
-        adj.addEdge(1, 2);
-        adj.addEdge(0, 2);
+        AdjacencyList adj = new AdjacencyList(false, 4);
         adj.addEdge(0, 1);
+        adj.addEdge(1, 2);
+        adj.addEdge(2, 3);
 
         adj.printGraph();
     }
-}       
+} 
